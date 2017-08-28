566107dd442363bcd562b174c50cdefca1053f53
maxwen <max.weninger@gmail.com>
2017-08-28 17:15:28 +0200

    Global Actions with Advanced Reboot [3/3]
    mydongistiny: Change setting for lockdown
    Change-Id: I601b4fb4a25e528767bcad7861446c2e8cdd377d

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -177,4 +177,6 @@ public class Settings extends SettingsActivity {
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
 
+    // Custom dashboard settings
+    public static class GlobalActionsSettingsActivity extends SettingsActivity { /* empty */ }
 }
